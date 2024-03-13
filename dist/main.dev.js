9bba920446bdfbf294b35bf94aacc72b2cf3d09b
tiago <tiagopinzgraffunder@gmail.com>
2024-03-13 10:31:59 -0300

    adionando lógica de programação na conversão de temperaturas

--- dist/main.dev.js
+++ dist/main.dev.js
@@ -8,7 +8,14 @@ var buttonLightTheme = document.getElementById("lightButton"); //variáveis
 var body = document.getElementById("bodyID");
 var root = document.querySelector(":root");
 var dark = document.getElementById("darkButton");
-var light = document.getElementById("lightButton"); //transformando light para dark
+var light = document.getElementById("lightButton");
+var button = document.getElementById("transform");
+var temperatureList = document.getElementById("selectTemperatureList");
+var temperatureInput = document.getElementById("temperature");
+var toConvertList = document.getElementById("selectToList");
+var display = document.getElementById("displayID");
+var result = 0;
+var currentExpression = ""; //transformando light para dark
 
 darkButton.addEventListener("click", function () {
   if (body.dataset.theme === "light") {
@@ -30,4 +37,52 @@ lightButton.addEventListener("click", function () {
     body.style.setProperty("--color-blue-standard", "#17a7a8");
     body.style.setProperty("--color-standard-two", "#ebe7e0");
   }
+}); //quando botão submit é clicado
+
+button.addEventListener("click", function (event) {
+  event.preventDefault(); //limpando ao fazer uma nova operação
+
+  display.value = "";
+
+  if (body.dataset.theme === "dark") {
+    display.style.backgroundColor = "#122f51";
+  } else if (body.dataset.theme === "light") {
+    display.style.backgroundColor = "#17a7a8";
+  } //pegando valores dos inputs
+
+
+  var degree = temperatureList.value;
+  var number = parseFloat(temperatureInput.value).toFixed(2);
+  var toConvertTemperature = toConvertList.value; //caso for celsius
+
+  if (degree === "C") {
+    if (toConvertTemperature === "toFahrenheit") {
+      display.value = "".concat(number * 1.8 + 32, "\xB0F");
+    } else if (toConvertTemperature === "toKelvin") {
+      display.value = "".concat(parseFloat(number) + 273.15, "K");
+    } else {
+      display.style.backgroundColor = "#e84624";
+      display.value = "erro: Escala não encontrada";
+    }
+  } //caso for fahrenheit
+  else if (degree === "F") {
+      if (toConvertTemperature === "toCelsius") {
+        display.value = "".concat((parseFloat(number) - 32) * 5 / 9, "\xB0C");
+      } else if (toConvertTemperature === "toKelvin") {
+        display.value = "".concat((parseFloat(number) - 32) * 5 / 9 + 273.15, "K");
+      } else {
+        display.style.backgroundColor = "#e84624";
+        display.value = "erro: Escala não encontrada";
+      }
+    } //caso for kelvin
+    else if (degree === "K") {
+        if (toConvertTemperature === "toFahrenheit") {
+          display.value = "".concat((parseFloat(number) - 273.15) * 9 / 5 + 32, "\xB0F");
+        } else if (toConvertTemperature === "toCelsius") {
+          display.value = "".concat(parseFloat(number) - 273.15, "\xB0C");
+        } else {
+          display.style.backgroundColor = "#e84624";
+          display.value = "erro: Escala não encontrada";
+        }
+      }
 });
--- main.js
+++ main.js
@@ -7,6 +7,13 @@ const body = document.getElementById("bodyID");
 const root = document.querySelector(":root");
 const dark = document.getElementById("darkButton");
 const light = document.getElementById("lightButton");
+const button = document.getElementById("transform");
+const temperatureList = document.getElementById("selectTemperatureList");
+const temperatureInput = document.getElementById("temperature");
+const toConvertList = document.getElementById("selectToList");
+const display = document.getElementById("displayID");
+let result = 0;
+let currentExpression = "";
 //transformando light para dark
 darkButton.addEventListener("click", () => {
   if (body.dataset.theme === "light") {
@@ -27,5 +34,54 @@ lightButton.addEventListener("click", () => {
     body.style.color = "#0d0c0c";
     body.style.setProperty("--color-blue-standard", "#17a7a8");
     body.style.setProperty("--color-standard-two", "#ebe7e0");
+  }
+});
+//quando botão submit é clicado
+button.addEventListener("click", (event) => {
+  event.preventDefault();
+  //limpando ao fazer uma nova operação
+  display.value = "";
+  if (body.dataset.theme === "dark") {
+    display.style.backgroundColor = "#122f51";
+  } else if (body.dataset.theme === "light") {
+    display.style.backgroundColor = "#17a7a8";
+  }
+  //pegando valores dos inputs
+  let degree = temperatureList.value;
+  let number = parseFloat(temperatureInput.value).toFixed(2);
+  let toConvertTemperature = toConvertList.value;
+
+  //caso for celsius
+  if (degree === "C") {
+    if (toConvertTemperature === "toFahrenheit") {
+      display.value = `${number * 1.8 + 32}°F`;
+    } else if (toConvertTemperature === "toKelvin") {
+      display.value = `${parseFloat(number) + 273.15}K`;
+    } else {
+      display.style.backgroundColor = "#e84624";
+      display.value = "erro: Escala não encontrada";
+    }
+  }
+  //caso for fahrenheit
+  else if (degree === "F") {
+    if (toConvertTemperature === "toCelsius") {
+      display.value = `${((parseFloat(number) - 32) * 5) / 9}°C`;
+    } else if (toConvertTemperature === "toKelvin") {
+      display.value = `${((parseFloat(number) - 32) * 5) / 9 + 273.15}K`;
+    } else {
+      display.style.backgroundColor = "#e84624";
+      display.value = "erro: Escala não encontrada";
+    }
+  }
+  //caso for kelvin
+  else if (degree === "K") {
+    if (toConvertTemperature === "toFahrenheit") {
+      display.value = `${((parseFloat(number) - 273.15) * 9) / 5 + 32}°F`;
+    } else if (toConvertTemperature === "toCelsius") {
+      display.value = `${parseFloat(number) - 273.15}°C`;
+    } else {
+      display.style.backgroundColor = "#e84624";
+      display.value = "erro: Escala não encontrada";
+    }
   }
 });
